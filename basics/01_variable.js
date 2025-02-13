63853e82ea1aabfdec0f253458ac560875f74f17
Nidhi Sharma <nidhisharma199700@gmail.com>
2025-02-13 18:07:12 +0000

    variable file

---
+++ basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId = 12345
+let accountMail = "nidhi@gmail.com"
+var accountPassword =  "123445"
+accountPlace = "Jaipur"
+let accountstate
+
+accountMail = "nidhi23@gmail.com"
+accountPassword =  "12344567"
+accountPlace = "Kota"
+//accountId = 2 not allowed
+console.log(accountId);
+
+console.table([accountId,accountMail,accountPassword,accountPlace,accountstate])
+
+/*
+Prefer not to use var because of block scope issue
+We can decalare variable without talking about type
+*/
