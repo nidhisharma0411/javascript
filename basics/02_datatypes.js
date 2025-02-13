b374f54135f440481524b4c2804e82e51986ede0
Nidhi Sharma <nidhisharma199700@gmail.com>
2025-02-13 18:21:42 +0000

    datatype

---
+++ basics/02_datatypes.js
@@ -0,0 +1,5 @@
+"use strict"; //just to know that whatever I am doing is as per standards
+
+//alert(3+3) //we are using nodejs not browser that's why I am getting error
+console.log(typeof(undefined));
+console.log(typeof(null));
